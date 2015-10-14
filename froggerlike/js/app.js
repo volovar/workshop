abc26038bd0b6793201ed1fa127ea455050c6b2c
Michael Volovar <mike@volovar.com>
2015-10-14 00:22:39 -0700

    some game engine work

--- froggerlike/js/app.js
+++ froggerlike/js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -24,13 +26,42 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (x, y) {
+    this.sprite = 'images/char-cat-girl.png';
+    this.x = x;
+    this.y = y;
+};
 
+Player.prototype = {
+    update: function () {
+
+    },
+    render: function () {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    },
+    handleInput: function (keyCode) {
+        if (keyCode === 'up') {
+            this.y -= 88;
+        } else if (keyCode === 'down') {
+            this.y += 88;
+        } else if (keyCode === 'left') {
+            this.x -= 120;
+        } else if (keyCode === 'right') {
+            this.x += 120;
+        }
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+allEnemies.push(new Enemy(1,200));
+allEnemies.push(new Enemy(180,200));
+allEnemies.push(new Enemy(100,50));
 
-
+console.log(allEnemies);
+var player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
