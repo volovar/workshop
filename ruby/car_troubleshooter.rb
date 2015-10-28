42715da7d30fe99444ae6de578541301df8b1481
Michael Volovar <mike@volovar.com>
2015-10-27 21:34:08 -0700

    more work on branching paths / car troubleshooter

--- ruby/car_troubleshooter.rb
+++ ruby/car_troubleshooter.rb
@@ -1,8 +1,23 @@
 # starting this
-print "Is the car silent when you turn the key? "
-
-if gets.chomp = 'y'
-    print "Are the battery terminals corroded? "
-elsif gets.chomp = 'good'
-    print "poop emoji"
+class String
+    def get_first_char
+        self[0,1].upcase
+    end
 end
+
+def find_next_path answer
+    if answer.get_first_char == 'Y'
+        true
+    elsif answer.get_first_char == 'N'
+        false
+    end
+end
+
+def ask_question text
+    print text
+    answer = gets.chomp
+end
+
+first_answer = ask_question "Is the car silent when you turn the key? "
+
+puts find_next_path(first_answer)
