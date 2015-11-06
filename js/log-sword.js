20e535dfcbe5894d64ea4c4cd0b12e5f6886ca4a
volovar <mike@volovar.com>
2015-11-05 16:50:19 -0800

    added a console log sword thing in case of a need to stand out in a sea of console logs

---
+++ js/log-sword.js
@@ -0,0 +1,8 @@
+// something easy to spot in a potential sea of console logs
+<script>
+    console.log('                                           !]')
+    console.log('____________________________________________]]')
+    console.log('\\___________________________________________]]//\\\\//\\\\//\\\\//\\\\(O)  ')
+    console.log('                                            ]]')
+    console.log('                                           !]')
+</script>
